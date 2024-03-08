b87493cab8ac208a13171016960a60ce2dcb91e0
Yeonghwan Jang <70074446+yeong-hwan@users.noreply.github.com>
2024-03-08 23:47:33 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,18 @@
+N = int(input())
+M = int(input())
+
+S = input()
+
+P = 'I'
+
+for _ in range(N):
+    P += "OI"
+
+result = 0
+
+for idx in range(M-len(P)+1):
+    check = S[idx:idx+len(P)]
+    if check == P:
+        result += 1
+    
+print(result)
